647669a2dfca52b198fed8b937fcb2f66936b7ca
mseguel <miseguel@uc.cl>
2022-03-25 22:08:36 -0300

    feat: retweeted y dias

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# twitter_stats
+# Twitter Stats
+Entrega estadisticas como:
+1. Los top 10 tweets más retweeted.
+2. Los top 10 usuarios en función a la cantidad de tweets que emitieron.
+3. Los top 10 días donde hay más tweets.
+4. Top 10 hashtags más usados
+
+## Funcionamineto
+- Se debe clonar el repositorio
+- Dentro de la carpeta del repositorio, crear una nueva carpeta que se llame ```data``` y agregar el archivo json que contiene la informacion de los twitts (```farmers-protest-tweets-2021-03-5.json```)
+-  Ejecutar el archivo ```main.ipynb``` en jupyter notebook
--- functions.py
+++ functions.py
@@ -4,8 +4,7 @@ import json
 
 def retweeted(dataset):  # 1. Los top 10 tweets más retweeted.
     print('Los 10 tweets más retweeted son:')
-    print(dataset)
-    # print(dataset.head())
+    print(dataset.nlargest(n=10, columns=['retweetCount'])[['retweetCount', 'content']])
 
 
 # 2. Los top 10 usuarios en función a la cantidad de tweets que emitieron.
@@ -15,10 +14,41 @@ def usuarios(dataset):
 
 def dias(dataset):  # 3. Los top 10 días donde hay más tweets.
     print("Los 10 días donde hay más tweets son:")
+    dataset['fecha'] = [d.date() for d in dataset['date']]
+    fechas_count = dataset['fecha'].value_counts()
+    top_fechas = pd.DataFrame({'Fecha':fechas_count.index, 'Frecuencia':fechas_count.values})
+    # top_fechas.columns = []
+    print(top_fechas.nlargest(n=10, columns=['Frecuencia'])[['Fecha', 'Frecuencia']].to_string(index=False))
+    # print(top_fechas.nlargest(n=10, columns = ['']))
+    # print(dataset['fecha'])
 
 
 def usados(dataset):  # 4. Top 10 hashtags más usados
     print("Los 10 hashtags más usados son:")
+
+
+# "url": "https://twitter.com/ShashiRajbhar6/status/1376739399593910273", 
+# "date": "2021-03-30T03:33:46+00:00", 
+# "content": "Support \ud83d\udc47\n\n#FarmersProtest", 
+# "renderedContent": "Support \ud83d\udc47\n\n#FarmersProtest", 
+# "id": 1376739399593910273, 
+# "user": {"username": "ShashiRajbhar6", "displayname": "Shashi Rajbhar", "id": 1015969769760096256, "description": "Satya presan \ud83e\udd14ho Sakta but prajit\ud83d\udcaa nhi\njhuth se samjhauta kbhi nhi\nJai Shree Ram \ud83d\udd49 \ud83d\ude4f\ud83d\udd49 followed by hon'ble @ArunrajbharSbsp", "rawDescription": "Satya presan \ud83e\udd14ho Sakta but prajit\ud83d\udcaa nhi\njhuth se samjhauta kbhi nhi\nJai Shree Ram \ud83d\udd49 \ud83d\ude4f\ud83d\udd49 followed by hon'ble @ArunrajbharSbsp", "descriptionUrls": [], "verified": false, "created": "2018-07-08T14:44:03+00:00", "followersCount": 1788, "friendsCount": 1576, "statusesCount": 14396, "favouritesCount": 26071, "listedCount": 1, "mediaCount": 254, "location": "Azm Uttar Pradesh, India", "protected": false, "linkUrl": null, "linkTcourl": null, "profileImageUrl": "https://pbs.twimg.com/profile_images/1354331299868237825/eDzdhZTD_normal.jpg", "profileBannerUrl": "https://pbs.twimg.com/profile_banners/1015969769760096256/1613727783", "url": "https://twitter.com/ShashiRajbhar6"}, 
+# "outlinks": [], 
+# "tcooutlinks": [], 
+# "replyCount": 0, 
+# "retweetCount": 0, 
+# "likeCount": 0, 
+# "quoteCount": 0, 
+# "conversationId": 1376739399593910273, 
+# "lang": "en", 
+# "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>", 
+# "sourceUrl": "http://twitter.com/download/android", 
+# "sourceLabel": "Twitter for Android", 
+# "media": null, 
+# "retweetedTweet": null, 
+# "quotedTweet": null, 
+# "mentionedUsers": null}
+
 
 
 if __name__ == "__main__":
@@ -29,15 +59,16 @@ if __name__ == "__main__":
     #     data = json.load(json_file)
     # print(data)
 
-    f = open('data/farmers-protest-tweets-2021-03-5.json', "r")
 
     # Reading from file
-    data = json.loads(f.read())
+    # f = open('data/farmers-protest-tweets-2021-03-5.json', "r")
+    # data = json.loads(f.read())
 
-    # data = pd.read_json(
-    # 'data/farmers-protest-tweets-2021-03-5.json', lines=True)
+    # data = pd.read_json('data/farmers-protest-tweets-2021-03-5.json', lines=True)
+    data = pd.read_json('data/test.json')
+    # print(data['user'].iloc[3])
 
-    # retweeted(data)
+    retweeted(data)
     # usuarios(datsa)
     # dias(data)
     # usados(data)
